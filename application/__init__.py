196c32bad406acb45123043ec96986fb5c78ccee
Hiski Ruuska <hiski.r@gmail.com>
2020-03-19 16:06:49 +0200

    accounts, login and logout

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chores.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 app.config["SQLALCHEMY_ECHO"] = True
 
@@ -13,5 +13,24 @@ from application import views
 
 from application.chores import models
 from application.chores import views
+
+from application.auth import models
+from application.auth import views
+
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 db.create_all()
