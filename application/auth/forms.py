ec94615893836d16f58e4c8eb7a58fe7bf7fc994
Hiski Ruuska <hiski.r@gmail.com>
2020-03-19 17:18:40 +0200

    added registration

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
   
+    class Meta:
+        csrf = False
+
+class RegistrationForm(FlaskForm):
+    name = StringField("name", [validators.Length(min=2)])
+    username = StringField("username", [validators.Length(min=3)])
+    password = PasswordField("password", [validators.Length(min=4)])
+
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,8 +1,25 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegistrationForm
+
+
+@app.route("/auth/register", methods=["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("/auth/registrationform.html", form=RegistrationForm())
+
+    form = RegistrationForm(request.form)
+
+    if not form.validate:
+        return render_template("/auth/registrationform.html", form=form)
+
+    u = User(form.name.data, form.username.data, form.password.data)
+    db.session().add(u)
+    db.session().commit()
+
+    return render_template("auth/loginform.html", form=LoginForm())
 
 
 @app.route("/auth/login", methods=["GET", "POST"])
@@ -11,7 +28,6 @@ def auth_login():
         return render_template("auth/loginform.html", form=LoginForm())
 
     form = LoginForm(request.form)
-    # mahdolliset validoinnit
 
     user = User.query.filter_by(
         username=form.username.data, password=form.password.data).first()
